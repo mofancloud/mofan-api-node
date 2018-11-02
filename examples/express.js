ee966179f58a55ce6de7b43c862d77002481108c
newtalentxp_ali <52388483@qq.com>
2018-11-02 20:04:21 +0800

    f

--- examples/express.js
+++ examples/express.js
@@ -6,7 +6,7 @@ var APP_ID = 'GNIw1EadTI2DEmRvx5HaRQ';
 var path = require('path');
 
 // 设置 api_key
-const XAPI = require('../lib/purple-cloud-api.js')(API_KEY)
+const XAPI = require('../lib/mofan-cloud-api.js')(API_KEY)
 XAPI.setPrivateKeyPath(path.join(__dirname, './rsa_private_key.pem'));
 const privateKey = XAPI.getPrivateKey()
 console.log(privateKey)
